aca35edcfbcef7a7ed5a2fc2df02f07cbf1d7d9c
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-03-04 20:10:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,50 @@
-const CACHE_NAME ='site-static';
-const CONTENTS =[
-    '/',
+var cacheName = 'food receipe app v1'; 
+
+var cacheAssets = [ 
+  '/',
     '/index.html',
     '/app.js',
     '/main.js',
     '/style.css',
-    '/img/192.png',
-    '/img/512.png'
-];
-self.addEventListener("install",e =>{
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(CONTENTS);
+    '/img/logo192.png',
+    '/img/logo512.png'
+]; 
 
-        })
-    );
-});
-self.addEventListener("fetch",e =>{
-    e.respondWith(
-        fetch(e.request).catch(()=>{
-             return caches.open(CACHE_NAME).then(cache => {
-                return cache.match(e.request);
-             })
-        })
-        
-    );
-});
+  
+
+self.addEventListener('install', e => { 
+
+    e.waitUntil( 
+        caches.open(cacheName) 
+        .then(cache => { 
+            console.log(`Service Worker: Caching Files: ${cache}`); 
+            cache.addAll(cacheAssets) 
+                .then(() => self.skipWaiting())
+        }) 
+    ); 
+}) 
+
+
+self.addEventListener('activate', e => { 
+    console.log('Service Worker: Activated'); 
+    e.waitUntil( 
+        caches.keys().then(cacheNames => { 
+            return Promise.all( 
+                cacheNames.map( 
+                    cache => { 
+                        if (cache !== cacheName) { 
+                            console.log('Service Worker: Clearing Old Cache'); 
+                            return caches.delete(cache); 
+                        } 
+                    } 
+                ) 
+
+            ) 
+
+        }) 
+
+    ); 
+}) 
+
+   
+   
